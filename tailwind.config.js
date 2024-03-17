633456ed8dbf8cba914f13c14b1a1224f38ebf05
Michael Obeng <msquireobeng@gmail.com>
2024-03-17 13:58:34 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      prime: '#282828',
+      card_bg: '#f2f2f2',
+      white: '#ffffff',
+      text_grey: '#d1d1d1',
+      text_primary: '#d1d1d1',
+      black: '#222831',
+      highlight: '#ffbc10',
+      transparent: 'transparent'
+    },
+    gridTemplateColumns: {
+      'tapbarmenu': '1fr 1fr 1fr 1fr 1fr',
+      'category_grid': 'repeat(3, minmax(0, 1fr))',
+      'featured_grid': 'repeat(2, minmax(0, 1fr))'
+    },
+    gridTemplateRows: {
+      'tapbarmenu': '100%',
+      'dashboard_services': 'repeat(4, minmax(0, 1fr))',
+      'cars_grid': '1fr 1fr',
+      'habits': '1fr 1fr 1fr 1fr'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
